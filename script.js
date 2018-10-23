763d7656c4e43aeb284e20d0c65ae92686f79dce
Jony33Invent <35571614+Jony33Invent@users.noreply.github.com>
2018-10-23 01:24:39 -0200

    mudei a grossura da onda

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var DesenhaGrade = function () {
             //Desenha a grade
             var i, qtdQuadrados = 5;
             ctx.beginPath();
-            ctx.lineWidth = 2;
+            ctx.lineWidth = 0.5;
             ctx.strokeStyle = '#EEEEEE';
             for (i = -qtdQuadrados; i <= qtdQuadrados; i++) {
                 ctx.moveTo((LARGURA / 2) - (LARGURA / (qtdQuadrados*2)) * i, 0);
@@ -68,7 +68,8 @@ var DesenhaGrade = function () {
                     document.querySelector('.btn').disabled=false;
                  }
             }
-
+            
+            ctx.lineWidth=4;
             ctx.stroke();
         }
 
